bcc54b9bf767cd29fdeb9d449742bf4b0e8bb2b0
alumno <alumno>
2019-02-22 11:30:51 -0300

    Terminar recu 5 del parcial de prueba

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+	var formadepago;
+	var paquete;
+	var precio;
+	var descuento;
+	var preciocondescuento;
+
+	precio=prompt("Ingrese el precio de la habitación para calcular el descuento");
+	paquete=prompt("Ingrese el paquete elegido (solo desayunos ó todo incluido)");
+	formadepago=prompt("Ingrese forma de pago");
+
+		switch(formadepago)
+			{
+				case "visa":
+				descuento=precio*10/100;
+				break;
+
+				case "paypal":
+				descuento=precio*15/100;
+
+
+			}
 
 }
